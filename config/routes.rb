135b691a9bedbe7ec20c26fea54cb6e401129b32
jasca18 <juan.santa_cruz@outlook.com>
2017-08-04 00:18:29 -0400

    no me funcionó la vista parcial con el formulario :(

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'todos/new'
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
-  get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todos'
   patch 'todos/:id', to: 'todos#update'
   delete 'todos/:id', to: 'todos#destroy'
   get 'todos/:id/complete', to: 'todos#complete', as: 'complete'
