e73a765e0ef2925bd834b7089119b1bf9b9890a5
jasca18 <juan.santa_cruz@outlook.com>
2017-08-03 23:58:30 -0400

    Prueba Rails JSC

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  get 'todos/list'
+  get 'todos', to: 'todos#index'
+  get 'todos/new'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get 'todos/:id/complete', to: 'todos#complete', as: 'complete'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
